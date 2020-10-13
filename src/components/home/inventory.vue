a0cf3a30318686515a577d97df2087346c3d4ecb
kangdongwei <1067905926@qq.com>
2020-10-13 17:46:40 +0800

    添加仓库首页表格样式

--- src/components/home/inventory.vue
+++ src/components/home/inventory.vue
@@ -55,14 +55,39 @@
             :data="tableData"
             style="width: 100%"
             background
-            height=500
+            height="500"
             :row-class-name="tableRowClassName"
           >
-            <el-table-column prop="date" label="日期" width="180">
+            <el-table-column type="expand">
+              <template slot-scope="props">
+                <el-form label-position="left" inline class="demo-table-expand">
+                  <el-form-item label="商品名称">
+                    <span>{{ props.row.name }}</span>
+                  </el-form-item>
+                  <el-form-item label="所属店铺">
+                    <span>{{ props.row.shop }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品 ID">
+                    <span>{{ props.row.id }}</span>
+                  </el-form-item>
+                  <el-form-item label="店铺 ID">
+                    <span>{{ props.row.shopId }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品分类">
+                    <span>{{ props.row.category }}</span>
+                  </el-form-item>
+                  <el-form-item label="店铺地址">
+                    <span>{{ props.row.address }}</span>
+                  </el-form-item>
+                  <el-form-item label="商品描述">
+                    <span>{{ props.row.desc }}</span>
+                  </el-form-item>
+                </el-form>
+              </template>
             </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址"> </el-table-column>
+            <el-table-column label="商品 ID" prop="id"> </el-table-column>
+            <el-table-column label="商品名称" prop="name"> </el-table-column>
+            <el-table-column label="描述" prop="desc"> </el-table-column>
           </el-table>
         </div>
         <div>
@@ -95,64 +120,40 @@ export default {
       total: 0,
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          id: "12987122",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          id: "12987123",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
         },
         {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          id: "12987125",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
         },
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
+          id: "12987126",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
         },
       ],
     };
@@ -261,7 +262,7 @@ body {
           }
         }
       }
-      
+
       .el-card__body {
         > div {
           .el-tree {
