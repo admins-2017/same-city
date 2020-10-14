dc29fef35eece4da86312d0958790717f508eb6b
kangdongwei <1067905926@qq.com>
2020-10-14 18:01:53 +0800

    添加仓库新功能

--- src/components/home/inventory.vue
+++ src/components/home/inventory.vue
@@ -23,26 +23,67 @@
     <div>
       <el-card class="box-card">
         <div slot="header" class="clearfix">
-          <span>库存明细</span>
-          <el-button style="float: right; padding: 3px 0" type="text"
-            >导出结果</el-button
-          >
+          <span>{{ shopName }}</span>
+          <div>
+            <el-button style="float: right; padding: 3px 0" type="text"
+              >导出结果</el-button
+            >
+            <el-button
+              style="float: right; padding: 3px 0"
+              type="text"
+              v-if="shopId != ''"
+              @click="changeQuery"
+              >取消查询</el-button
+            >
+          </div>
         </div>
         <div>
           <div>
-            <el-row>
-              <el-col
-                :span="4"
-                v-for="(o, index) in 4"
-                :key="o"
-                :offset="index > 0 ? 2 : 0"
-              >
-                <el-card :body-style="{ padding: '0px' }">
-                  <div style="padding: 14px">
-                    <span>好吃的汉堡</span>
-                    <div class="bottom clearfix">
-                      <time class="time">{{ currentDate }}</time>
-                      <el-button type="text" class="button">操作按钮</el-button>
+            <el-row :gutter="20">
+              <el-col :span="6">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <div class="my-card-context" @click="getCommodity">
+                    <div>
+                      <span>商品总量</span>
+                    </div>
+                    <div>
+                      <span>{{ commodityNumber }}</span>
+                    </div>
+                  </div>
+                </el-card>
+              </el-col>
+              <el-col :span="6">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <div class="my-card-context" @click="getZero">
+                    <div>
+                      <span>缺货商品</span>
+                    </div>
+                    <div>
+                      <span>{{ stockNumber }}</span>
+                    </div>
+                  </div>
+                </el-card>
+              </el-col>
+              <el-col :span="6">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <div class="my-card-context" @click="getWarn">
+                    <div>
+                      <span>存量不足</span>
+                    </div>
+                    <div>
+                      <span>{{ warnNumber }}</span>
+                    </div>
+                  </div>
+                </el-card>
+              </el-col>
+              <el-col :span="6">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <div class="my-card-context" @click="getAmple">
+                    <div>
+                      <span>存量充足</span>
+                    </div>
+                    <div>
+                      <span>{{ inventoryNumber }}</span>
                     </div>
                   </div>
                 </el-card>
@@ -55,39 +96,46 @@
             :data="tableData"
             style="width: 100%"
             background
-            height="500"
             :row-class-name="tableRowClassName"
           >
             <el-table-column type="expand">
               <template slot-scope="props">
                 <el-form label-position="left" inline class="demo-table-expand">
                   <el-form-item label="商品名称">
-                    <span>{{ props.row.name }}</span>
+                    <span>{{ props.row.commodityName }}</span>
                   </el-form-item>
                   <el-form-item label="所属店铺">
-                    <span>{{ props.row.shop }}</span>
+                    <span>{{ props.row.shopName }}</span>
                   </el-form-item>
-                  <el-form-item label="商品 ID">
-                    <span>{{ props.row.id }}</span>
+                  <el-form-item label="所属分类">
+                    <span>{{ props.row.classificationName }}</span>
                   </el-form-item>
-                  <el-form-item label="店铺 ID">
-                    <span>{{ props.row.shopId }}</span>
+                  <el-form-item label="商品简介">
+                    <span>{{ props.row.commodityDescription }}</span>
                   </el-form-item>
-                  <el-form-item label="商品分类">
-                    <span>{{ props.row.category }}</span>
+                  <el-form-item label="商品售价">
+                    <span>{{ props.row.commoditySellingPrice }}</span>
                   </el-form-item>
-                  <el-form-item label="店铺地址">
-                    <span>{{ props.row.address }}</span>
+                  <el-form-item label="商品规格">
+                    <span>{{ props.row.commodityUnit }}</span>
                   </el-form-item>
-                  <el-form-item label="商品描述">
-                    <span>{{ props.row.desc }}</span>
+                  <el-form-item label="库存数量">
+                    <span style="color: SaddleBrown; font-weight: 600">{{
+                      props.row.inventoryNumber
+                    }}</span>
+                  </el-form-item>
+                  <el-form-item label="商铺地址">
+                    <span>{{ props.row.shopAddress }}</span>
                   </el-form-item>
                 </el-form>
               </template>
             </el-table-column>
-            <el-table-column label="商品 ID" prop="id"> </el-table-column>
-            <el-table-column label="商品名称" prop="name"> </el-table-column>
-            <el-table-column label="描述" prop="desc"> </el-table-column>
+            <el-table-column label="商铺名称" prop="shopName">
+            </el-table-column>
+            <el-table-column label="商品名称" prop="commodityName">
+            </el-table-column>
+            <el-table-column label="库存数量" prop="inventoryNumber">
+            </el-table-column>
           </el-table>
         </div>
         <div>
@@ -97,6 +145,26 @@
             layout="total, prev, pager, next, jumper"
             :total="total"
           ></el-pagination>
+        </div>
+        <div>
+          <el-dialog :title="dialogTitle" :visible.sync="dialogTableVisible">
+            <el-table :data="gridData">
+              <el-table-column
+                property="date"
+                label="日期"
+                width="150"
+              ></el-table-column>
+              <el-table-column
+                property="name"
+                label="姓名"
+                width="200"
+              ></el-table-column>
+              <el-table-column
+                property="address"
+                label="地址"
+              ></el-table-column>
+            </el-table>
+          </el-dialog>
         </div>
       </el-card>
     </div>
@@ -118,50 +186,36 @@ export default {
       size: 10,
       page: 1,
       total: 0,
-      tableData: [
-        {
-          id: "12987122",
-          name: "好滋好味鸡蛋仔",
-          category: "江浙小吃、小吃零食",
-          desc: "荷兰优质淡奶，奶香浓而不腻",
-          address: "上海市普陀区真北路",
-          shop: "王小虎夫妻店",
-          shopId: "10333",
-        },
-        {
-          id: "12987123",
-          name: "好滋好味鸡蛋仔",
-          category: "江浙小吃、小吃零食",
-          desc: "荷兰优质淡奶，奶香浓而不腻",
-          address: "上海市普陀区真北路",
-          shop: "王小虎夫妻店",
-          shopId: "10333",
-        },
-        {
-          id: "12987125",
-          name: "好滋好味鸡蛋仔",
-          category: "江浙小吃、小吃零食",
-          desc: "荷兰优质淡奶，奶香浓而不腻",
-          address: "上海市普陀区真北路",
-          shop: "123",
-          shopId: "10333",
-        },
-        {
-          id: "12987126",
-          name: "好滋好味鸡蛋仔",
-          category: "江浙小吃、小吃零食",
-          desc: "荷兰优质淡奶，奶香浓而不腻",
-          address: "上海市普陀区真北路",
-          shop: "testtest",
-          shopId: "10333",
-        },
-      ],
+      tableData: [],
+      shopId: "",
+      shopName: "库存明细",
+      warnNumber: "",
+      commodityNumber: "",
+      stockNumber: "",
+      inventoryNumber: "",
+      dialogTitle: "",
+      gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false,
     };
   },
   methods: {
     filterNode(value, shopList) {
-      console.log(value);
-      console.log(shopList);
       if (!value) return true;
       return shopList.shopName.indexOf(value) !== -1;
     },
@@ -181,38 +235,108 @@ export default {
           this.$message.error("服务器连接超时 请重试！");
         });
     },
+    getCommodityByShop() {
+      let getUrl = "/api/inventory?page=" + this.page + "&size=" + this.size;
+      if (this.shopId != "") {
+        getUrl = getUrl + "&shopId=" + this.shopId;
+      }
+      axios({
+        method: "get",
+        url: getUrl,
+        headers: {
+          Authorization: this.userDetails.token,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          this.warnNumber = res.data.data.warnNumber;
+          this.commodityNumber = res.data.data.commodityNumber;
+          this.stockNumber = res.data.data.stockNumber;
+          this.inventoryNumber = res.data.data.inventoryNumber;
+          this.total = res.data.data.vos.total;
+          this.tableData = res.data.data.vos.records;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$message.error("服务器连接超时 请重试！");
+        });
+    },
     handleNodeClick(val) {
-      console.log(val);
-      //   this.dialogDetailTableVisible = true;
+      this.shopName = val.shopName;
+      this.page = 0;
+      this.shopId = val.shopId;
+      this.getCommodityByShop();
+    },
+    changeQuery() {
+      this.page = 0;
+      this.shopId = "";
+      this.shopName = "库存明细";
+      this.getCommodityByShop();
     },
     tableRowClassName({ row, rowIndex }) {
-      console.log(row);
-      if (rowIndex === 1) {
+      rowIndex;
+      if (row.inventoryNumber < 10 && row.inventoryNumber > 0) {
         return "warning-row";
-      } else if (rowIndex === 3) {
-        return "success-row";
+      } else if (row.inventoryNumber === 0) {
+        return "error-row";
       }
-      return "";
+      return "success-row";
     },
     handleCurrentChange(val) {
       this.page = val;
-      let isNoll = false;
-      for (var key in this.findCommodityForm) {
-        if (this.findCommodityForm[key] != "") {
-          isNoll = true;
-        }
-      }
-      console.log(isNoll);
-      if (isNoll) {
-        this.findCommodityByCondition();
-      } else {
-        this.getAllCommodity();
-      }
+      this.getCommodityByShop();
     },
+    getDetails(val) {
+      let getUrl = "/api/inventory/status/"+val;
+      if (this.shopId != "") {
+        getUrl = getUrl + "&shopId=" + this.shopId;
+      }
+      console.log(getUrl)
+      axios({
+        method: "get",
+        url: getUrl,
+        headers: {
+          Authorization: this.userDetails.token,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$message.error("服务器连接超时 请重试！");
+        });
+    },
+    getCommodity() {
+      this.dialogTableVisible = true;
+      this.dialogTitle = "商品总量"
+      this.getDetails(1);
+    },
+    getWarn() {
+      this.dialogTableVisible = true;
+      this.dialogTitle = "缺货商品"
+      this.getDetails(2);
+
+    },
+    getZero() {
+      this.dialogTableVisible = true;
+      this.dialogTitle = "存量不足"
+      this.getDetails(3);
+
+    },
+    getAmple() {
+      this.dialogTableVisible = true;
+      this.dialogTitle = "存量充足"
+      this.getDetails(4);
+
+    },
+    
   },
   created() {
     this.userDetails = JSON.parse(localStorage.getItem("user-information"));
+    console.log(this.userDetails);
     this.getShop();
+    this.getCommodityByShop();
   },
   watch: {
     filterText(val) {
@@ -229,13 +353,16 @@ body {
   height: 100%;
   margin: 0px;
   padding: 0px;
+  overflow-y: hidden;
 }
 #commoditys {
   width: 100%;
-  height: 95%;
+  height: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  overflow-y: hidden;
+
   > div:nth-of-type(1) {
     width: 22%;
     height: 100%;
@@ -244,9 +371,46 @@ body {
     .box-card {
       width: 95%;
       height: 100%;
-      overflow-y: auto;
       position: relative;
       .el-card__header {
+        padding: 10px;
+        height: 10%;
+        .clearfix {
+          padding: 10px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          span {
+            padding: 8px;
+          }
+          .el-button {
+            padding: 8px;
+            font-size: 13px;
+          }
+        }
+      }
+
+      .el-card__body {
+        > .el-tree {
+          margin: 10px -10px;
+        }
+      }
+    }
+  }
+  > div:nth-of-type(2) {
+    width: 78%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: white;
+    .box-card {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      overflow-y: hidden;
+      .el-card__header {
+        height: 10%;
+        width: 100%;
         padding: 10px;
         .clearfix {
           padding: 10px;
@@ -264,142 +428,68 @@ body {
       }
 
       .el-card__body {
-        > div {
-          .el-tree {
-            margin: 10px 0px;
-          }
-        }
-      }
-    }
-  }
-  > div:nth-of-type(2) {
-    width: 78%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    background-color: white;
-    > .box-card {
-      width: 100%;
-      height: 100%;
-      position: relative;
-      overflow-y: auto;
-      .el-card__header {
-        padding: 10px;
-        .clearfix {
-          padding: 10px;
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          span {
-            padding: 8px;
-          }
-          .el-button {
-            padding: 8px;
-            font-size: 13px;
-          }
-        }
-      }
-      .my-collapse {
-        width: 100%;
+        widows: 100%;
         height: 80%;
-        overflow-y: auto;
-        .el-collapse {
-          width: 100%;
-          height: 100%;
-          .el-collapse-item__wrap {
-            width: 100%;
-            height: 100%;
-
-            .el-collapse-item__content {
-              width: 100%;
+        > div:nth-of-type(1) {
+          widows: 100%;
+          height: 10%;
+          > div {
+            .my-card-context {
               display: flex;
-              justify-content: space-between;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              background-color: #f8f8ff;
               > div:nth-of-type(1) {
-                width: 25%;
-                img {
-                  width: 90%;
-                  height: 160px;
-                }
+                margin-top: 10px;
+                font-weight: 600;
               }
               > div:nth-of-type(2) {
-                width: 75%;
-                img {
-                  width: 100%;
-                  height: 210px;
-                }
-                .operating-commodity {
-                  width: 50%;
-                  margin: 10px 0px;
-                  display: flex;
-                  justify-content: space-between;
-                }
+                margin: 10px 0px;
+                font-size: 15px;
               }
             }
           }
         }
-      }
-      .el-table .warning-row {
-        background: rgb(179, 202, 74);
-      }
+        > div:nth-of-type(2) {
+          widows: 100%;
+          height: 68%;
+          .el-table {
+            height: 100%;
+            overflow-y: auto;
+          }
+          .el-table .success-row {
+            background: white;
+          }
+          .el-table .warning-row {
+            background: rgb(255 239 219);
+          }
 
-      .el-table .success-row {
-        background: #f0f9eb;
-      }
-      .el-pagination {
-        margin-bottom: 10px;
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        position: relative;
-        bottom: -20px;
-      }
-    }
-    .el-drawer {
-      width: 100%;
-      .el-drawer__body {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        > div {
-          width: 80%;
-          height: 100%;
-          margin: 20px 0px;
-          .avatar-uploader .el-upload {
-            border: 1px dashed #d9d9d9;
-            border-radius: 6px;
-            cursor: pointer;
-            position: relative;
-            overflow: hidden;
-            // display: flex;
-            // justify-content: center;
-            // align-items: center;
+          .el-table .error-row {
+            background: rgb(255 106 106);
           }
-          .avatar-uploader .el-upload:hover {
-            border-color: #409eff;
+          margin-top: 10px;
+          .demo-table-expand {
+            font-size: 0;
           }
-          .avatar-uploader-icon {
-            font-size: 28px;
-            color: #8c939d;
-            width: 100%;
-            height: 200px;
-            line-height: 178px;
-            text-align: center;
+          .demo-table-expand label {
+            width: 90px;
+            color: #99a9bf;
           }
-          .avatar {
-            width: 100%;
-            height: 200px;
-            display: block;
-          }
-          .el-select {
-            width: 100%;
+          .demo-table-expand .el-form-item {
+            margin-right: 0;
+            margin-bottom: 0;
+            width: 50%;
           }
         }
-      }
-    }
-    .add-commodity {
-      .el-upload {
-        width: 100%;
+        > div:nth-of-type(3) {
+          widows: 100%;
+          height: 10%;
+          margin-top: 10px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
       }
     }
   }
